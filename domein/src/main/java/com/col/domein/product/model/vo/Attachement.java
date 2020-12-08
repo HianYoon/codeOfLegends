e201a70210b8bbb057dd7f667a12e716736827d7
jihun-z <wkftodrlswlgns82@gmail.com>
2020-12-08 19:47:50 +0900

    #product.객체생성

---
+++ domein/src/main/java/com/col/domein/product/model/vo/Attachement.java
@@ -0,0 +1,17 @@
+package com.col.domein.product.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Attachement {
+	private int product_No;
+	private int product_image_No;
+	
+
+}
