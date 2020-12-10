8a23348df6ac03f199d2c18fc8a0dc45efadbbfa
Byung-hyun Yoon <mightymosses@gmail.com>
2020-12-10 23:04:00 +0900

    병현/풀을 위한 커밋!

--- domein/src/main/java/com/col/domein/HomeController.java
+++ domein/src/main/java/com/col/domein/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Handles requests for the application home page.
@@ -32,7 +33,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
 		return "index";
 	}
 	
