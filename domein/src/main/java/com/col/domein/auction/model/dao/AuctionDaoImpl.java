52bd7d77b03dd52681ae55389570f6a17edb4872
jihun-z <wkftodrlswlgns82@gmail.com>
2021-01-05 03:34:28 +0900

    #auction하는중

--- domein/src/main/java/com/col/domein/auction/model/dao/AuctionDaoImpl.java
+++ domein/src/main/java/com/col/domein/auction/model/dao/AuctionDaoImpl.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.col.domein.auction.model.vo.BoardAttachementFile;
+import com.col.domein.auction.model.vo.BoardAttachementImage;
+import com.col.domein.auction.model.vo.BoardAuction;
 @Repository
 public class AuctionDaoImpl implements AuctionDao {
 
@@ -13,6 +17,24 @@ public class AuctionDaoImpl implements AuctionDao {
 	public List<Map> selectBusinessKey(SqlSession session, int memberKey) {
 		// TODO Auto-generated method stub
 		return session.selectList("boardAuction.selectBusinessKey",memberKey);
+	}
+	//auction등록
+	@Override
+	public int inertEnllo(SqlSession session, BoardAuction auc) {
+		// TODO Auto-generated method stub
+		return session.insert("boardAuction.inertEnllo",auc);
+	}
+	//파일
+	@Override
+	public int insertUpFile(SqlSession session, BoardAttachementFile bFile) {
+		// TODO Auto-generated method stub
+		return session.insert("boardAuction.insertUpFile",bFile);
+	}
+	//이미지
+	@Override
+	public int insertImgFile(SqlSession session, BoardAttachementImage bImg) {
+		// TODO Auto-generated method stub
+		return session.insert("boardAuction.insertImgFile",bImg);
 	}
 
 }
--- domein/src/main/java/com/col/domein/auction/model/service/AuctionService.java
+++ domein/src/main/java/com/col/domein/auction/model/service/AuctionService.java
@@ -3,10 +3,16 @@ package com.col.domein.auction.model.service;
 import java.util.List;
 import java.util.Map;
 
+import com.col.domein.auction.model.vo.BoardAttachementFile;
+import com.col.domein.auction.model.vo.BoardAttachementImage;
+import com.col.domein.auction.model.vo.BoardAuction;
 import com.col.domein.business.model.vo.Business;
 
 public interface AuctionService {
 	//등록페이지사업자 정보불러오기
 	List<Map> selectBusinessKey(int memberKey);
+	//등록
+	int inertEnllo(BoardAuction auc, List<BoardAttachementImage> imgs, List<BoardAttachementFile> files);
+
 
 }
